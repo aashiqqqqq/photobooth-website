1f25f1498d1213ffac2c00028d159d7eb4618d27
aashiqqqqq <mdaashiqnawaz@gmail.com>
2026-01-12 02:52:45 +0530

    fix deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/photobooth-website/',
   plugins: [react()],
   resolve: {
     alias: {
